46bc2ed5a6151d89c28750f245490bfee6d25199
Jaykant <jaykantnayak07@gmail.com>
2024-06-21 01:35:41 +0530

    Starting Scene Done

---
+++ Assets/Scripts/ScreenManager.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScreenManager : MonoBehaviour
+{
+    public Canvas Screen1;
+    public Canvas Screen2;
+    public Canvas Screen3;
+    public Canvas Screen4;
+    void Start()
+    {
+        Screen1.gameObject.SetActive(true);
+        Screen2.gameObject.SetActive(false);
+        Screen3.gameObject.SetActive(false);
+        Screen4.gameObject.SetActive(false);
+    }
+    public void ShowScene2()
+    {
+        Screen1.gameObject.SetActive(false);
+        Screen2.gameObject.SetActive(true);
+        Screen3.gameObject.SetActive(false);
+        Screen4.gameObject.SetActive(false);
+    }
+    public void ShowScene3()
+    {
+        Screen1.gameObject.SetActive(false);
+        Screen2.gameObject.SetActive(false);
+        Screen3.gameObject.SetActive(true);
+        Screen4.gameObject.SetActive(false);
+    }
+    public void ShowScene4()
+    {
+        Screen1.gameObject.SetActive(false);
+        Screen2.gameObject.SetActive(false);
+        Screen3.gameObject.SetActive(false);
+        Screen4.gameObject.SetActive(true);
+    }
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+    
+    public void Instruction()
+    {
+        SceneManager.LoadScene(6);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    
+}
