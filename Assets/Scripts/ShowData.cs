b2b8cf23d213a36906c254f890d81c694f779464
Ishan Kamboj <ISHANKAMBOJ35@GMAIL.COM>
2024-06-23 13:08:37 +0530

    partially made show data scene

--- Assets/Scripts/ShowData.cs
+++ Assets/Scripts/ShowData.cs
@@ -16,38 +16,57 @@ public class ShowData : MonoBehaviour
     public TextMeshProUGUI card1Name;
     public Image card1Player1Image;
     public Image card1Player2Image;
-    public TextMeshProUGUI waterResult1;
-    public TextMeshProUGUI infraResult1;
-
+    public TextMeshProUGUI waterResult1Player1;
+    public TextMeshProUGUI infraResult1Player1;
+    public TextMeshProUGUI waterResult1Player2;
+    public TextMeshProUGUI infraResult1Player2;
 
     public TextMeshProUGUI card2Name;
     public Image card2Player1Image;
     public Image card2Player2Image;
-    public TextMeshProUGUI waterResult2;
-    public TextMeshProUGUI infraResult2;
+    public TextMeshProUGUI waterResult2Player1;
+    public TextMeshProUGUI infraResult2Player1;
+    public TextMeshProUGUI waterResult2Player2;
+    public TextMeshProUGUI infraResult2Player2;
 
 
     public TextMeshProUGUI card3Name;
     public Image card3Player1Image;
     public Image card3Player2Image;
-    public TextMeshProUGUI waterResult3;
-    public TextMeshProUGUI infraResult3;
+    public TextMeshProUGUI waterResult3Player1;
+    public TextMeshProUGUI infraResult3Player1;
+    public TextMeshProUGUI waterResult3Player2;
+    public TextMeshProUGUI infraResult3Player2;
 
     public TextMeshProUGUI card4Name;
     public Image card4Player1Image;
     public Image card4Player2Image;
-    public TextMeshProUGUI waterResult4;
-    public TextMeshProUGUI infraResult4;
+    public TextMeshProUGUI waterResult4Player1;
+    public TextMeshProUGUI infraResult4Player1;
+    public TextMeshProUGUI waterResult4Player2;
+    public TextMeshProUGUI infraResult4Player2;
+
+
     public TextMeshProUGUI roundScore1;
     public TextMeshProUGUI roundScore2;
 
-    public TextMeshProUGUI totalScore1;
-    public TextMeshProUGUI totalScore2;
+    public TextMeshProUGUI[] totalScore1;
+    public TextMeshProUGUI[] totalScore2;
     public TextMeshProUGUI totalInfra1;
     public TextMeshProUGUI totalInfra2;
     public TextMeshProUGUI GW1;
     public TextMeshProUGUI GW2;
 
+    public TextMeshProUGUI infraAddedP1;
+    public TextMeshProUGUI infraAddedP2;
+
+    public TextMeshProUGUI GWUsedP1;
+    public TextMeshProUGUI GWUsedP2;
+
+    int infraInCurrentRoundP1;
+    int infraInCurrentRoundP2;
+    int GroundWaterUsedInCurrentRoundP1;
+    int GroundWaterUsedInCurrentRoundP2;
     private Card[] cards;
     void Start()
     {
@@ -70,15 +89,35 @@ public class ShowData : MonoBehaviour
         SetImage(GameManager.choices[3, 0] == 1 ? "COOPERATE" : "DEFECT", card4Player1Image);
         SetImage(GameManager.choices[3, 1] == 1 ? "COOPERATE" : "DEFECT", card4Player2Image);
 
-        waterResult1.text=GameManager.currentRoundWater[0,0].ToString()+"   "+GameManager.currentRoundWater[0,1].ToString();
-        waterResult2.text=GameManager.currentRoundWater[1,0].ToString()+"   "+GameManager.currentRoundWater[1,1].ToString();
-        waterResult3.text=GameManager.currentRoundWater[2,0].ToString()+"   "+GameManager.currentRoundWater[2,1].ToString();
-        waterResult4.text=GameManager.currentRoundWater[3,0].ToString()+"   "+GameManager.currentRoundWater[3,1].ToString();
+        waterResult1Player1.text = GameManager.currentRoundWater[0, 0].ToString();
+        waterResult1Player2.text = GameManager.currentRoundWater[0,1].ToString();
+        waterResult2Player1.text = GameManager.currentRoundWater[1, 0].ToString();
+        waterResult2Player2.text = GameManager.currentRoundWater[1, 1].ToString();
+        waterResult3Player1.text = GameManager.currentRoundWater[2, 0].ToString();
+        waterResult3Player2.text = GameManager.currentRoundWater[2, 1].ToString();
+        waterResult4Player1.text = GameManager.currentRoundWater[3, 0].ToString();
+        waterResult4Player2.text = GameManager.currentRoundWater[3, 1].ToString();
 
-        infraResult1.text=GameManager.currentRoundInfra[0,0].ToString()+"   "+GameManager.currentRoundInfra[0,1].ToString();
-        infraResult2.text=GameManager.currentRoundInfra[1,0].ToString()+"   "+GameManager.currentRoundInfra[1,1].ToString();
-        infraResult3.text=GameManager.currentRoundInfra[2,0].ToString()+"   "+GameManager.currentRoundInfra[2,1].ToString();
-        infraResult4.text=GameManager.currentRoundInfra[3,0].ToString()+"   "+GameManager.currentRoundInfra[3,1].ToString();
+        infraResult1Player1.text = GameManager.currentRoundInfra[0, 0].ToString();
+        infraResult1Player2.text = GameManager.currentRoundInfra[0,1].ToString();
+        infraResult2Player1.text = GameManager.currentRoundInfra[1, 0].ToString();
+        infraResult2Player2.text = GameManager.currentRoundInfra[1, 1].ToString();
+        infraResult3Player1.text = GameManager.currentRoundInfra[2, 0].ToString();
+        infraResult3Player2.text = GameManager.currentRoundInfra[2, 1].ToString();
+        infraResult4Player1.text = GameManager.currentRoundInfra[3, 0].ToString();
+        infraResult4Player2.text = GameManager.currentRoundInfra[3, 1].ToString();
+
+        //infraInCurrentRound[0] = GameManager.currentRoundInfra[0, 0] + GameManager.currentRoundInfra[1, 0] + GameManager.currentRoundInfra[2, 0] + GameManager.currentRoundInfra[3, 0];
+        infraInCurrentRoundP2 = GameManager.currentRoundInfra[0, 1] + GameManager.currentRoundInfra[1, 1] + GameManager.currentRoundInfra[2, 1] + GameManager.currentRoundInfra[3, 1];
+        infraInCurrentRoundP1 = GameManager.currentRoundInfra[0, 0] + GameManager.currentRoundInfra[1, 0] + GameManager.currentRoundInfra[2, 0] + GameManager.currentRoundInfra[3, 0];
+        GroundWaterUsedInCurrentRoundP1 = GameManager.currentRoundWater[0, 0] + GameManager.currentRoundWater[1, 0] + GameManager.currentRoundWater[2, 0] + GameManager.currentRoundWater[3, 0];
+        GroundWaterUsedInCurrentRoundP2 = GameManager.currentRoundWater[0, 1] + GameManager.currentRoundWater[1, 1] + GameManager.currentRoundWater[2, 1] + GameManager.currentRoundWater[3, 1];
+
+        infraAddedP1.text = "+" + infraInCurrentRoundP1.ToString();
+        infraAddedP2.text = "+" + infraInCurrentRoundP2.ToString();
+
+        GWUsedP1.text = "-" + GroundWaterUsedInCurrentRoundP1.ToString();
+        GWUsedP2.text = "-" + GroundWaterUsedInCurrentRoundP2.ToString();
 
         float score1 = 0, score2 = 0;
         for (int i = 0; i < 4; i++)
@@ -89,13 +128,17 @@ public class ShowData : MonoBehaviour
         roundScore1.text = score1.ToString();
         roundScore2.text = score2.ToString();
 
-        totalScore1.text = GameManager.player1Score.ToString();
-        totalScore2.text = GameManager.player2Score.ToString();
+        totalScore1[0].text = GameManager.player1Score.ToString();
+        totalScore2[0].text = GameManager.player2Score.ToString();
+        totalScore1[1].text = GameManager.player1Score.ToString();
+        totalScore2[1].text = GameManager.player2Score.ToString();
         totalInfra1.text = GameManager.player1Infra.ToString();
         totalInfra2.text = GameManager.player2Infra.ToString();
         GW1.text = GameManager.player1Groundwater.ToString();
         GW2.text = GameManager.player2Groundwater.ToString();
-
+        player1Region.text = GameManager.player1Region.name.Split(' ')[0];
+        player2Region.text = GameManager.player2Region.name.Split(' ')[0];
+        //Debug.Log(player1Region.text);
     }
     public void SetImage(string txt, Image img)
     {
